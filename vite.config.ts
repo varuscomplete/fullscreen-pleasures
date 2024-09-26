fa64cb3221ccc5b3516616e7ae7b7ab0e7265a5c
varuscomplete <varuscomplete@gmail.com>
2024-09-26 15:37:29 +0000

    package changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import glsl from "vite-plugin-glsl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/unknown-pleasures",
+  base: "/fullscreen-heavyweight",
   plugins: [react(), glsl()],
 });
